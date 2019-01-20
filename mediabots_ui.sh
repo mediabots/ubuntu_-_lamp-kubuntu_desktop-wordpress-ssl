a496da4ebe13f0257f2521a799c5fb342bf400dc
MediaBOTS <46617144+mediabots@users.noreply.github.com>
2019-01-20 09:29:36 +0100

    Update mediabots_ui.sh

--- mediabots_ui.sh
+++ mediabots_ui.sh
@@ -6,12 +6,12 @@ wordpress_installation_=0
 read -r -p "[1] Do you want to install Wordpress? (Yes/No) : " wordpress_installation
 wordpress_installation=$(echo "$wordpress_installation" | head -c 1)
 
-if [ -z "$wordpress_installation" ] && [ "$wordpress_installation" = 'Y' -o "$wordpress_installation" = 'y' ]; then
+if [ ! -z $wordpress_installation ] && [ $wordpress_installation = 'Y' -o $wordpress_installation = 'y' ]; then
 	wordpress_installation_=1
 	echo ".:Advanced Setting:. It is Optional"
 	read -r -p "Do you want to setup Wordpress Database? (Yes/No) : " wpdb_choice
 	wpdb_choice=$(echo "$wpdb_choice" | head -c 1)
-	if [ "$wpdb_choice" = 'Y' -o "$wpdb_choice" = 'y' ]; then
+	if [ $wpdb_choice = 'Y' -o $wpdb_choice = 'y' ]; then
 		read -r -p " [1.1] Enter Wordpress 'Database Name', you want to create : " wpdb_name
 		read -r -p " [1.2] Enter Wordpress 'Database Username', you want to create : " wpdb_user
 		read -r -p " [1.3] Enter Wordpress 'Database Password', you want to create : " wpdb_password
@@ -19,19 +19,19 @@ if [ -z "$wordpress_installation" ] && [ "$wordpress_installation" = 'Y' -o "$wo
 fi
 
 read -r -p "[2] Enter Domain Name (leave Blank,if you don't have any) : " domain
-if [ -z "$domain" ] && [ $wordpress_installation_ -eq 1 ]; then
+if [ -z $domain ] && [ $wordpress_installation_ -eq 1 ]; then
 	domain='wordpress'
 	host=0
 fi
 
-if [ -z "$domain" ] && [ $wordpress_installation_ -eq 0 ]; then
+if [ -z $domain ] && [ $wordpress_installation_ -eq 0 ]; then
 	domain='mysite'
 	host=0
 fi
 
-if [ -z "$wpdb_name" ]; then wpdb_name='wordpress_DATABASE'; fi
-if [ -z "$wpdb_user" ]; then wpdb_user='wordpress_USER'; fi
-if [ -z "$wpdb_password" ]; then wpdb_password='wordpress_PASSWORD'; fi
+if [ -z $wpdb_name ]; then wpdb_name='wordpress_DATABASE'; fi
+if [ -z $wpdb_user ]; then wpdb_user='wordpress_USER'; fi
+if [ -z $wpdb_password ]; then wpdb_password='wordpress_PASSWORD'; fi
 
 echo "[3] Do you want to install Remote Desktop(XRDP)?"
 read -r -p " > It would take at least 1 hour to install. (Yes/No) : " xrdp_installtion
@@ -44,14 +44,12 @@ printf "Y\n" | apt install sudo -y
 sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y upgrade
 sudo apt-get -y dist-upgrade
-sleep 5
 
-if [ "$xrdp_installtion" = 'Y' -o "$xrdp_installtion" = 'y' ]; then
+if [ $xrdp_installtion = 'Y' -o $xrdp_installtion = 'y' ]; then
 	# Installing Kubuntu Desktop & its Dependencies
 	sudo apt-get -y install xorg xrdp build-essential tasksel
 	sudo DEBIAN_FRONTEND=noninteractive tasksel install kubuntu-desktop
 	sudo service xrdp restart
-	sleep 10
 
 	# Setting Up Kubuntu Desktop
 	sudo apt-get -y install nemo gedit
@@ -62,7 +60,6 @@ if [ "$xrdp_installtion" = 'Y' -o "$xrdp_installtion" = 'y' ]; then
 	sudo rm -f /*/*/Desktop/trash.desktop
 	sudo apt-get autoclean
 	sudo apt-get autoremove
-	sleep 10
 
 	# Installing WINE to run Windows Applications
 	sudo dpkg --add-architecture i386
@@ -72,7 +69,6 @@ if [ "$xrdp_installtion" = 'Y' -o "$xrdp_installtion" = 'y' ]; then
 	sudo apt-get update
 	sudo apt-get install --install-recommends winehq-devel -y
 	sudo apt-get install winetricks -y
-	sleep 5
 fi
 
 # Installing LAMP (Apache Server, MySQL, Php) & Firewall
@@ -87,7 +83,6 @@ sudo mysql -e "FLUSH PRIVILEGES"
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y install php libapache2-mod-php php-mysql
 sudo service apache2 restart
 sudo chown -R "$USER":root /var/www
-sleep 10
 
 # Preparing PhpMyAdmin installation
 sudo apt-get -y install debconf-utils
@@ -108,7 +103,6 @@ CMD_EOF
 
 sudo echo -en "[mysql]\nuser=root\npassword=mysql_PASSWORD\n" >~/.my.cnf
 sudo chmod 0600 ~/.my.cnf
-sleep 10
 
 # Preparing Website
 mkdir /var/www/html/$domain
@@ -121,7 +115,6 @@ sudo a2dissite 000-default.conf
 sudo systemctl reload apache2
 sudo a2enmod rewrite
 sudo systemctl restart apache2
-sleep 10
 
 if [ $wordpress_installation_ -eq 1 ]; then
 	# Creating Wordpress Database
@@ -140,17 +133,26 @@ CMD_EOF
 
 	# Pre Configuring Wordpress
 	sudo cp ./wordpress/wp-config-sample.php ./wordpress/wp-config.php
+	sudo sed -i '/put your unique phrase here/d' ./wordpress/wp-config.php # delete matching pattern
+	sudo sed -i '$d' ./wordpress/wp-config.php # delete last line
+	table_prefix=$(tr </dev/urandom -dc A-Za-z | head -c6)_ || WP_
+	sudo sed -i 's/wp_/'$table_prefix'/g' ./wordpress/wp-config.php
 	sudo sed -i 's/database_name_here/'$wpdb_name'/g' ./wordpress/wp-config.php
 	sudo sed -i 's/username_here/'$wpdb_user'/g' ./wordpress/wp-config.php
 	sudo sed -i 's/password_here/'$wpdb_password'/g' ./wordpress/wp-config.php
-	sudo sed -i "s/require_once(ABSPATH . 'wp-settings.php');/define('FS_METHOD', 'direct');\r\nrequire_once(ABSPATH . 'wp-settings.php');/g" ./wordpress/wp-config.php
-
-	# Generating SALT keys
-	for key in $(grep 'put your unique phrase here' ./wordpress/wp-config.php | cut -f2 -d"'"); do
-		RAND_KEY=$(tr </dev/urandom -dc _A-Z-a-z-0-9 | head -c32) || true
-		sudo sed -i "s/define(\s*'${key}',\s*'put your unique phrase here'\s*);/define('${key}', '${RAND_KEY}');/" ./wordpress/wp-config.php
+	# Setting up SALT [ Thanks to @paulwaldmann]
+	sudo curl -s https://api.wordpress.org/secret-key/1.1/salt/ > ./wordpress/keys.txt
+	keys=($(grep 'define' ./wordpress/keys.txt | cut -f2 -d"'"))
+	oldIFS=$IFS
+	IFS=$'\n'
+	idx=0
+	for value in $(grep 'define' ./wordpress/keys.txt | cut -f4 -d"'"); do 
+	sudo echo -en "define('${keys[$idx]}', '${value}');\r\n" >> ./wordpress/wp-config.php; 
+	idx=$((idx+1))
 	done
-
+	IFS=$oldIFS
+	sudo echo -en "define('FS_METHOD', 'direct');\r\nrequire_once(ABSPATH . 'wp-settings.php');" >> ./wordpress/wp-config.php; #Update WordPress Directly Without Using FTP
+	
 	# Installing & Configuring Wordpress
 	sudo rsync -avP ./wordpress/ /var/www/html/$domain/
 	sudo chown -R www-data:www-data /var/www/html/$domain/
@@ -160,20 +162,30 @@ CMD_EOF
 	sudo find /var/www/html/$domain -type d -exec chmod 750 {} \;
 	sudo find /var/www/html/$domain -type f -exec chmod 640 {} \;
 
-	# Install Command line interface for WordPress (WP-CLI)
+	# Install Command line interface for WordPress (WP-CLI) [ credit @paulwaldmann]
 	curl https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar >/tmp/wp-cli.phar
 	chmod +x /tmp/wp-cli.phar
 	mv /tmp/wp-cli.phar /usr/local/bin/wp
 fi
-sleep 10
 
-# Cleaning Data
+# Hiding Server info
+#sudo sed -i "0,/<\/Directory>/{s/<\/Directory>/<\/Directory>\n<Directory \/var\/www\/html>\n\tOptions -Indexes\n<\/Directory>\n/}" /etc/apache2/apache2.conf
+sudo echo -en "ServerSignature off\nServerTokens prod" >> /etc/apache2/apache2.conf
+sudo service apache2 restart
+#sudo apt-get install libapache2-mod-security2 -y
+#sudo mv /etc/modsecurity/modsecurity.conf-recommended /etc/modsecurity/modsecurity.conf
+#sudo service apache2 reload
+#sudo sed -i "s/SecRuleEngine DetectionOnly/SecRuleEngine On/" /etc/modsecurity/modsecurity.conf
+#sudo sed -i "s/SecResponseBodyAccess On/SecResponseBodyAccess Off/" /etc/modsecurity/modsecurity.conf
+#sudo sed -i 's/<\/IfModule>/\tSecServerSignature " "\n<\/IfModule>/' /etc/apache2/mods-enabled/security2.conf # whether does it work ,test it after apache2 restart, by curl -I http://IP_Address
+#sudo service apache2 restart
+
+# Cleaning Data 
 if [ $wordpress_installation_ -eq 1 ]; then
 	rm -f /var/www/html/index.html
 	rm -rf wordpress
 fi
 rm -f ~/.my.cnf
-sleep 5
 
 # Renabling SSH
 sudo /etc/init.d/ssh restart
@@ -186,7 +198,6 @@ sudo dpkg-reconfigure openssh-server
 	chmod 755 ./10ssh
 )
 sudo systemctl enable ssh.service
-sleep 10
 
 # Adding Domain name in Host file
 if [ $host -eq 1 ]; then sudo echo -e "$(curl ifconfig.me)\t$domain" >>/etc/hosts; fi
@@ -201,7 +212,6 @@ if [ $host -eq 1 ]; then
 	sudo sed -i 's/<\/VirtualHost>/RewriteEngine on\nRewriteCond %{SERVER_NAME} ='$domain'\nRewriteRule ^ https:\/\/%{SERVER_NAME}%{REQUEST_URI} [END,NE,R=permanent]\n<\/VirtualHost>/g' /etc/apache2/sites-available/$domain.conf
 	sudo systemctl restart apache2
 fi
-sleep 5
 
 #sudo reboot
 # now open SITE_URL_or_IP/wp-admin/install.php
